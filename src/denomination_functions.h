697dfb41ec958a39f25ab2a2a64fb351b8d78f6a
armann1993 <armanbeglaryan19@gmail.com>
2019-07-20 14:02:34 +0300

    Denomination update

--- src/denomination_functions.h
+++ src/denomination_functions.h
@@ -11,14 +11,14 @@
 #include "reverse_iterate.h"
 #include "util.h"
 #include "libzerocoin/Denominations.h"
-#include "primitives/zerocoin.h"
+#include "zpiv/zerocoin.h"
 #include <list>
 #include <map>
-std::vector<CZerocoinMint> SelectMintsFromList(const CAmount nValueTarget, CAmount& nSelectedValue,
+std::vector<CMintMeta> SelectMintsFromList(const CAmount nValueTarget, CAmount& nSelectedValue,
                                                int nMaxNumberOfSpends,
                                                bool fMinimizeChange,
                                                int& nCoinsReturned,
-                                               const std::list<CZerocoinMint>& listMints,
+                                               const std::list<CMintMeta>& listMints,
                                                const std::map<libzerocoin::CoinDenomination, CAmount> mapDenomsHeld,
                                                int& nNeededSpends
                                                );
@@ -30,4 +30,4 @@ int calculateChange(
     const std::map<libzerocoin::CoinDenomination, CAmount>& mapOfDenomsHeld,
     std::map<libzerocoin::CoinDenomination, CAmount>& mapOfDenomsUsed);
 
-void listSpends(const std::vector<CZerocoinMint>& vSelectedMints);
+void listSpends(const std::vector<CMintMeta>& vSelectedMints);
