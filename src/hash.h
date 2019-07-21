726784461b1fdddb35bf7c535a3a994583b9df38
armann1993 <armanbeglaryan19@gmail.com>
2019-07-21 03:38:29 +0300

    Update hash.h

--- src/hash.h
+++ src/hash.h
@@ -16,12 +16,10 @@
 #include "version.h"
 
 #include "crypto/sph_blake.h"
-#include "crypto/sph_bmw.h"
 #include "crypto/sph_groestl.h"
 #include "crypto/sph_jh.h"
 #include "crypto/sph_keccak.h"
 #include "crypto/sph_skein.h"
-#include "crypto/sha512.h"
 
 #include <iomanip>
 #include <openssl/sha.h>
@@ -29,8 +27,6 @@
 #include <vector>
 
 using namespace std;
-
-typedef uint256 ChainCode;
 
 /** A hasher class for Bitcoin's 256-bit hash (double SHA-256). */
 class CHash256
@@ -43,9 +39,9 @@ public:
 
     void Finalize(unsigned char hash[OUTPUT_SIZE])
     {
-        unsigned char buf[CSHA256::OUTPUT_SIZE];
+        unsigned char buf[sha.OUTPUT_SIZE];
         sha.Finalize(buf);
-        sha.Reset().Write(buf, CSHA256::OUTPUT_SIZE).Finalize(hash);
+        sha.Reset().Write(buf, sha.OUTPUT_SIZE).Finalize(hash);
     }
 
     CHash256& Write(const unsigned char* data, size_t len)
@@ -61,33 +57,7 @@ public:
     }
 };
 
-class CHash512
-{
-private:
-    CSHA512 sha;
 
-public:
-    static const size_t OUTPUT_SIZE = CSHA512::OUTPUT_SIZE;
-
-    void Finalize(unsigned char hash[OUTPUT_SIZE])
-    {
-        unsigned char buf[CSHA512::OUTPUT_SIZE];
-        sha.Finalize(buf);
-        sha.Reset().Write(buf, CSHA512::OUTPUT_SIZE).Finalize(hash);
-    }
-
-    CHash512& Write(const unsigned char* data, size_t len)
-    {
-        sha.Write(data, len);
-        return *this;
-    }
-
-    CHash512& Reset()
-    {
-        sha.Reset();
-        return *this;
-    }
-};
 /** A hasher class for Bitcoin's 160-bit hash (SHA-256 + RIPEMD-160). */
 class CHash160
 {
@@ -99,9 +69,9 @@ public:
 
     void Finalize(unsigned char hash[OUTPUT_SIZE])
     {
-        unsigned char buf[CSHA256::OUTPUT_SIZE];
+        unsigned char buf[sha.OUTPUT_SIZE];
         sha.Finalize(buf);
-        CRIPEMD160().Write(buf, CSHA256::OUTPUT_SIZE).Finalize(hash);
+        CRIPEMD160().Write(buf, sha.OUTPUT_SIZE).Finalize(hash);
     }
 
     CHash160& Write(const unsigned char* data, size_t len)
@@ -141,23 +111,6 @@ inline void Hash(void* in, unsigned int len, unsigned char* out)
     SHA256_Final(out, &sha256);
 }
 
-/** Compute the 512-bit hash of an object. */
-template <typename T1>
-inline uint512 Hash512(const T1 pbegin, const T1 pend)
-{
-    static const unsigned char pblank[1] = {};
-    uint512 result;
-    CHash512().Write(pbegin == pend ? pblank : (const unsigned char*)&pbegin[0], (pend - pbegin) * sizeof(pbegin[0])).Finalize((unsigned char*)&result);
-    return result;
-}
-template <typename T1, typename T2>
-inline uint512 Hash512(const T1 p1begin, const T1 p1end, const T2 p2begin, const T2 p2end)
-{
-    static const unsigned char pblank[1] = {};
-    uint512 result;
-    CHash512().Write(p1begin == p1end ? pblank : (const unsigned char*)&p1begin[0], (p1end - p1begin) * sizeof(p1begin[0])).Write(p2begin == p2end ? pblank : (const unsigned char*)&p2begin[0], (p2end - p2begin) * sizeof(p2begin[0])).Finalize((unsigned char*)&result);
-    return result;
-}
 
 /** Compute the 256-bit hash of an object. */
 template <typename T1>
