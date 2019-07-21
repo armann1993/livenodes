4be4f2d3bf707b37905fd89ddd92d7133bd7b2fd
armann1993 <armanbeglaryan19@gmail.com>
2019-07-21 03:27:22 +0300

    Update hash files

--- src/hash.cpp
+++ src/hash.cpp
@@ -77,7 +77,7 @@ void BIP32Hash(const ChainCode chainCode, unsigned int nChild, unsigned char hea
     num[1] = (nChild >> 16) & 0xFF;
     num[2] = (nChild >> 8) & 0xFF;
     num[3] = (nChild >> 0) & 0xFF;
-    CHMAC_SHA512(chainCode.begin(), chainCode.size()).Write(&header, 1).Write(data, 32).Write(num, 4).Finalize(output);
+    CHMAC_SHA512(chainCode, 32).Write(&header, 1).Write(data, 32).Write(num, 4).Finalize(output);
 }
 
 void scrypt_hash(const char* pass, unsigned int pLen, const char* salt, unsigned int sLen, char* output, unsigned int N, unsigned int r, unsigned int p, unsigned int dkLen)
